ecee9ac29234112e3842b6a9643f47ac6d662176
Pranilkha <ps24pkhatiwoda@efcts.us>
2023-05-12 10:13:17 -0400

    122121

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function openNav1() {
+    document.getElementById("mySidenav1").style.width = "100%";
+  }
+  
+  function closeNav1() {
+    document.getElementById("mySidenav1").style.width = "0";
+  }
