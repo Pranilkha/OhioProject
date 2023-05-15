7ebdb0b58ff978b518b9a4178b722aae27d684a4
Pranilkha <ps24pkhatiwoda@efcts.us>
2023-05-15 08:40:40 -0400

    bau

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,15 @@
 function openNav1() {
-    document.getElementById("mySidenav1").style.width = "100%";
+    document.getElementById("mySidenav1").style.width = "50%";
   }
   
   function closeNav1() {
     document.getElementById("mySidenav1").style.width = "0";
+  }
+
+  function openNav2() {
+    document.getElementById("mySidenav2").style.width = "100%";
+  }
+  
+  function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
   }
