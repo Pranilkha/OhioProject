8ba8ea02dd41dd5eebff0c93bd900bbbc14d15b3
Pranilkha <ps24pkhatiwoda@efcts.us>
2023-05-16 08:37:37 -0400

    ere

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,79 @@
 function openNav1() {
     document.getElementById("mySidenav1").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
   }
   
   function closeNav1() {
     document.getElementById("mySidenav1").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
   }
 
   function openNav2() {
-    document.getElementById("mySidenav2").style.width = "100%";
+    document.getElementById("mySidenav2").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
   }
   
   function closeNav2() {
     document.getElementById("mySidenav2").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
+
+  function openNav3() {
+    document.getElementById("mySidenav3").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
+  }
+  
+  function closeNav3() {
+    document.getElementById("mySidenav3").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
+
+  function openNav4() {
+    document.getElementById("mySidenav4").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
+  }
+  
+  function closeNav4() {
+    document.getElementById("mySidenav4").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
+
+  function openNav5() {
+    document.getElementById("mySidenav5").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
+  }
+  
+  function closeNav5() {
+    document.getElementById("mySidenav5").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
+
+  function openNav6() {
+    document.getElementById("mySidenav6").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
+  }
+  
+  function closeNav6() {
+    document.getElementById("mySidenav6").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
+
+  function openNav7() {
+    document.getElementById("mySidenav7").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
+  }
+  
+  function closeNav7() {
+    document.getElementById("mySidenav7").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
+
+  function openNav8() {
+    document.getElementById("mySidenav8").style.width = "50%";
+    document.getElementById("main").style.marginRight = "50%";
+  }
+  
+  function closeNav8() {
+    document.getElementById("mySidenav8").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
   }
